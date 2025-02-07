f21806f653e4a1f24b21e7c0a695326a4773e39d
NIDHI WALKE <bcs_2023041@iiitm.ac.in>
2025-02-07 18:09:43 +0530

    Updated  App.jsx

--- CodeSync-frontend/src/App.jsx
+++ CodeSync-frontend/src/App.jsx
@@ -7,10 +7,7 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <h>hi frtfg</h>
-    </>
+   <></>
   );
 }
 
